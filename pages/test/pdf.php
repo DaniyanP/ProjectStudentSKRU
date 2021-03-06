051aa904f023ea2ba00ad5129f68e110884b2433
DaniyanP <54161489+DaniyanP@users.noreply.github.com>
2021-03-06 23:42:39 +0700

    up

--- pages/test/pdf.php
+++ pages/test/pdf.php
@@ -1,62 +1,71 @@
+
 <?php
-  require_once __DIR__ . '/vendor/autoload.php';
-  include '../../conn.php';
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
+	//เรียกใช้ไฟล์ autoload.php ที่อยู่ใน Folder vendor
+	require_once __DIR__ . '../../vendor/autoload.php';
+	
+	//ตั้งค่าการเชื่อมต่อฐานข้อมูล
+	
+    include("conn.php");
+	
+	mysqli_set_charset($con, "utf8");
 
-  $sql = "SELECT * FROM tb_member";
-  $result = mysqli_query($conn, $sql);
-  $content = "";
-  if (mysqli_num_rows($result) > 0) {
-    $i = 1;
-    while($row = mysqli_fetch_assoc($result)) {
-      $tablebody .= '<tr style="border:1px solid #000;">
-        <td style="border-right:1px solid #000;padding:3px;text-align:center;"  >'.$i.'</td>
-        <td style="border-right:1px solid #000;padding:3px;">'.$row['username'].'</td>
-        <td style="border-right:1px solid #000;padding:3px;">'.$row['member_name'].'</td>
-        <td style="border-right:1px solid #000;padding:3px;">'.$row['member_lname'].'</td>
-        <td style="border-right:1px solid #000;padding:3px;">'.$row['email'].'</td>
-      </tr>';
-      $i++;
-    }
-  }
-  
-mysqli_close($conn);
-  
-$mpdf = new mPDF();
+	if (!$con) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
 
-$tableh = '
+	$sql = "SELECT * FROM items";
+	
+	$result = mysqli_query($con, $sql);
+	$content = "";
+	if (mysqli_num_rows($result) > 0) {
+		$i = 1;
+		while($row = mysqli_fetch_assoc($result)) {
+			$content .= '<tr style="border:1px solid #000;">
+				<td style="border-right:1px solid #000;padding:3px;text-align:center;"  >'.$i.'</td>
+				<td style="border-right:1px solid #000;padding:3px;text-align:center;" >'.$row['code'].'</td>
+				<td style="border-right:1px solid #000;padding:3px;"  >'.$row['name'].'</td>
+				<td style="border-right:1px solid #000;padding:3px;text-align:center;"  >'.$row['units'].'</td>
+				<td style="border-right:1px solid #000;padding:3px;text-align:right;"  >'.number_format($row['price'],2).'</td>
+			</tr>';
+			$i++;
+		}
+	}
+	
+	mysqli_close($con);
+	
+    $mpdf = new \Mpdf\Mpdf();
+
+$head = '
 <style>
-  body{
-    font-family: "Garuda"; 
-  }
+	body{
+		font-family: "Garuda";//เรียกใช้font Garuda สำหรับแสดงผล ภาษาไทย
+	}
 </style>
 
-<h2 style="text-align:center">List Member </h2>
+<h2 style="text-align:center">ใบรับสินค้า</h2>
 
 <table id="bg-table" width="100%" style="border-collapse: collapse;font-size:12pt;margin-top:8px;">
     <tr style="border:1px solid #000;padding:4px;">
         <td  style="border-right:1px solid #000;padding:4px;text-align:center;"   width="10%">ลำดับ</td>
-        <td  style="border-right:1px solid #000;padding:4px;text-align:center;"  width="15%">username</td>
-        <td  width="15%" style="border-right:1px solid #000;padding:4px;text-align:center;">&nbsp; ชื่อ </td>
-        <td  style="border-right:1px solid #000;padding:4px;text-align:center;"  width="15%">สกุล </td>
-        <td  style="border-right:1px solid #000;padding:4px;text-align:center;" width="15%">อีเมล์</td>
+        <td  style="border-right:1px solid #000;padding:4px;text-align:center;"  width="15%">รหัสสินค้า</td>
+        <td  width="45%" style="border-right:1px solid #000;padding:4px;text-align:center;">&nbsp;รายละเอียดสินค้า</td>
+        <td  style="border-right:1px solid #000;padding:4px;text-align:center;"  width="15%">หน่วยนับ</td>
+        <td  style="border-right:1px solid #000;padding:4px;text-align:center;" width="15%">ราคา (฿)</td>
     </tr>
 
 </thead>
-  <tbody>';
-  
-$tableend = "</tbody>
+	<tbody>';
+	
+$end = "</tbody>
 </table>";
 
-$mpdf->WriteHTML($tableh);
+$mpdf->WriteHTML($head);
 
-$mpdf->WriteHTML($tablebody);
+$mpdf->WriteHTML($content);
 
-$mpdf->WriteHTML($tableend);
+$mpdf->WriteHTML($end);
 
 $mpdf->Output();
 
-//https://monkeywebstudio.com/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B9%84%E0%B8%9F%E0%B8%A5%E0%B9%8C-pdf-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-mpdf/
-?>
+
+
